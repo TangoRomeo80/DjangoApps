291a67a95ba1aa134d4aa233e7cf79addea253a2
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-02-12 17:36:41 +0600

    updated models

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -14,6 +14,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -39,7 +40,6 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
