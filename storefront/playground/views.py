f5ea737a7c4684050c9ee51118f0d1c30228fef6
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-03 01:40:13 +0600

    reverted playground to default

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 def say_hello(request):
+
     return render(request, 'hello.html', {'name': 'Mosh'})
 
