33ef095631c94ba800c1233d6a6b56d586bd090d
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-03 16:08:56 +0600

    added order models

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -3,9 +3,19 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ['title', 'unit_price']
+  list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
   list_editable = ['unit_price']
   list_per_page = 10
+  list_select_related = ['collection']
+
+  def collection_title(self, product):
+    return product.collection.title
+
+  @admin.display(ordering='inventory')
+  def inventory_status(self, product):
+    if product.inventory < 10:
+      return 'Low'
+    return 'OK'
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -13,6 +23,10 @@ class CustomerAdmin(admin.ModelAdmin):
   list_editable = ['membership']
   ordering = ['first_name', 'last_name']
   list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+  list_display = ['id', 'placed_at', 'customer']
 
 admin.site.register(models.Collection)
 
