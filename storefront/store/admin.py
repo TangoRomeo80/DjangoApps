fe16cf903310b3e2c69bdf0c5970532d86708e75
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-05 01:44:04 +0600

    added filter to admin page

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
 from django.urls import reverse
-from django.utils.html import format_html
+from django.utils.html import format_html, urlencode
 from . import models
+
+class InventoryFilter(admin.SimpleListFilter):
+  title = 'inventory'
+  parameter_name = 'inventory'
+
+  def lookups(self, request, model_admin):
+    return [
+      ('<10', 'Low')
+    ]
+
+  def queryset(self, request, queryset):
+    if self.value() == '<10':
+      return queryset.filter(inventory__lt=10)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
   list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
   list_editable = ['unit_price']
+  list_filter = ['collection', 'last_update', InventoryFilter]
   list_per_page = 10
   list_select_related = ['collection']
 
@@ -24,8 +38,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
   list_display = ['first_name', 'last_name', 'membership']
   list_editable = ['membership']
-  ordering = ['first_name', 'last_name']
   list_per_page = 10
+  ordering = ['first_name', 'last_name']
+  search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -37,7 +52,11 @@ class CollectionAdmin(admin.ModelAdmin):
 
   @admin.display(ordering='products_count')
   def products_count(self, collection):
-    reverse('admin:store_product_changelist')
+    url = (reverse('admin:store_product_changelist') 
+          + '?'
+          + urlencode({
+            'collection__id': str(collection.id)
+          }))
     return format_html('<a href="{}">{}</a>', url, collection.products_count)
      
   def get_queryset(self, request):
