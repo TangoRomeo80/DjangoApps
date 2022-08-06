c456d529a4af209b4a862c8c745c88e29428f3ea
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-06 06:04:34 +0600

    added serializer with model sterilizers

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -3,18 +3,19 @@ from store.models import Product, Collection
 from rest_framework import serializers
 
 class CollectionSerializer(serializers.Serializer):
-  id = serializers.IntegerField()
-  title = serializers.CharField(max_length=255)
+  class Meta:
+    model = Collection
+    fields = ['id', 'title', 'products_count']
 
-class ProductSerializer(serializers.Serializer):
-  id = serializers.IntegerField()
-  title = serializers.CharField(max_length=255)
-  unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
-  price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-  collection = serializers.HyperlinkedRelatedField(
-    queryset=Collection.objects.all(),
-    view_name='collection-detail',
-  )
+  products_count = serializers.IntegerField()
 
+class ProductSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Product
+    fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
+  price_with_tax = serializers.SerializerMethodField(
+    method_name='calculate_tax')
+ 
   def calculate_tax(self, product: Product):
     return product.unit_price * Decimal(1.1)
