dcd613c002e8778a7997f1d3857ffb75ad4f4209
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-08 17:18:39 +0600

    implement nested routes

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
 from django.urls import include
-from rest_framework.routers import SimpleRouter
+# from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = SimpleRouter()
+
+# router = DefaultRouter()
+# router.register('products', views.ProductViewSet)
+# router.register('collections', views.CollectionViewSet)
+
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
 # URLConf
-urlpatterns = router.urls
+
+# urlpatterns = router.urls
+
+urlpatterns = router.urls + products_router.urls
 
 # urlpatterns = [
 #     path('products/', views.ProductList.as_view()),
