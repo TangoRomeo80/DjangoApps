9110047e478b6796b9be4164060b544dabea0b0b
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-19 01:31:24 +0600

    implemented getting cart items

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from store.models import Product, Collection, Review, Cart
+from store.models import Product, Collection, Review, Cart, CartItem
 from rest_framework import serializers
 
 
@@ -33,8 +33,30 @@ class ReviewSerializer(serializers.ModelSerializer):
         return Review.objects.create(product_id=product_id, **validated_data)
 
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField(method_name='calculate_total_price')
+
+    def calculate_total_price(self, cart_item:CartItem):
+        return cart_item.product.unit_price * cart_item.quantity
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'cart', 'product', 'quantity', 'total_price']
+        
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
+
+    def get_total_price(self, cart:Cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
     class Meta:
         model = Cart
-        fields = ['id']
+        fields = ['id', 'items', 'total_price']
