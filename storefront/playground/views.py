bd76379a0f979323ee2ef49dc3d227467571828f
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-11-13 21:14:58 +0600

    removed base files

--- storefront/playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
-
-
-def say_hello(request):
-
-    return render(request, 'hello.html', {'name': 'Mosh'})
-
