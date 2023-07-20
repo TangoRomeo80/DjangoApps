795c6f136c90a9345e865e2e8771ab5c31675052
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-21 01:44:55 +0600

    reset project

--- storefront/likes/models.py
+++ storefront/likes/models.py
@@ -3,12 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
 class LikedItem(models.Model):
-    # What user liked what object
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Type of object
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # Primary key of object
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
--- storefront/playground/apps.py
+++ storefront/playground/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LikesConfig(AppConfig):
+class PlaygroundConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'likes'
+    name = 'playground'
