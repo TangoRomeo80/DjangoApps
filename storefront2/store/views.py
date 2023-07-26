556da305e81e39841f1c375cc430a6099323b290
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-26 13:59:23 +0600

    completed upto creating api views

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# default django built in response
+# def product_list(request):
+#     return HttpResponse('OK')
+
+# rest framework response
+@api_view()
+def product_list(request):
+    return Response('OK')
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
