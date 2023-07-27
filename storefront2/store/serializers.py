5e58f131dc0072d86136963cc57d2608a536d925
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-27 14:00:16 +0600

    implement custom serializer fields

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from .models import Product
+from decimal import Decimal
 
 #Serializer is a class that converts model to JSON
 
@@ -6,4 +8,9 @@ from rest_framework import serializers
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
