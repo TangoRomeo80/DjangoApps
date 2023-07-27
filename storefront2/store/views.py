4e5fb60541b96773630621e394f0e29968f4138e
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-07-27 13:46:52 +0600

    implement serializing objects

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 # default django built in response
 # def product_list(request):
@@ -9,8 +13,28 @@ from rest_framework.response import Response
 # rest framework response
 @api_view()
 def product_list(request):
-    return Response('OK')
+    # get all products from database
+    queryset = Product.objects.all()
+    # convert products to JSON
+    serializer = ProductSerializer(queryset, many=True)
+    # return JSON
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    # Implementation with try/except
+    # try:
+    #     # get product from database
+    #     product = Product.objects.get(pk=id)
+    #     # convert product to JSON
+    #     serializer = ProductSerializer(product)
+    #     # return JSON
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     # return 404 if product does not exist
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # Implementation with get_object_or_404
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
