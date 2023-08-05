eddbd5c24e1efcc57bedf0ebbf3ab4a58b6076fa
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-08-06 00:41:06 +0600

    Complete dupto routers

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -1,18 +1,26 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+# When using viewsets, we are not going to explicitly create the URL patterns
+# Use routers instead
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
 # URLConf
-urlpatterns = [
-    # path('products/', views.product_list),
-    # path('products/<int:id>/', views.product_detail),
-    # path('collections/', views.collection_list),
-    # path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
-    # path('products/', views.ProductList.as_view()), # Class based view
-    # path('products/<int:pk>/', views.ProductDetail.as_view()), # Class based view, Generic view always takes pk
-    # path('collections/', views.CollectionList.as_view()), # Class based view
-    # path('collections/<int:pk>/', views.CollectionDetail.as_view()), # Class based view
-    path('products/', views.ProductList.as_view()), # Viewset
-    path('products/<int:pk>/', views.ProductDetail.as_view()), # viewset using pk
-    path('collections/', views.CollectionList.as_view()), # Viewset
-    path('collections/<int:pk>/', views.CollectionDetail.as_view()), # Viewset
-]
+urlpatterns = router.urls # This will generate the URL patterns for us
+
+# To also inclue custom URL patterns, we can do the following
+# urlpatterns = router.urls + [ Other paths here ]
+
+# urlpatterns = [
+#     # path('products/', views.product_list),
+#     # path('products/<int:id>/', views.product_detail),
+#     # path('collections/', views.collection_list),
+#     # path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
+#     # path('products/', views.ProductList.as_view()), # Class based view
+#     # path('products/<int:pk>/', views.ProductDetail.as_view()), # Class based view, Generic view always takes pk
+#     # path('collections/', views.CollectionList.as_view()), # Class based view
+#     # path('collections/<int:pk>/', views.CollectionDetail.as_view()), # Class based view
+# ]
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -8,7 +8,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 # from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import status
-from .models import Product, Collection
+from .models import OrderItem, Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # default django built in response
@@ -24,15 +24,21 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    # Override built in delete method
-    def delete(self, request, pk):
-        # get product from database
-        product = get_object_or_404(Product, pk=pk)
-        # Check if there is any order item with this product
-        if product.orderitems.count() > 0:
+    # Overridde the destroy method to check if product is associated with an order item
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # Override built in delete method
+    # def delete(self, request, pk):
+    #     # get product from database
+    #     product = get_object_or_404(Product, pk=pk)
+    #     # Check if there is any order item with this product
+    #     if product.orderitems.count() > 0:
+    #         return Response({'error': 'Product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 # rest framework response (generic view implementing mixins under the hood)
 # class ProductList(ListCreateAPIView):
@@ -196,18 +202,26 @@ class ProductViewSet(ModelViewSet):
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Implementation using viewsets
-class collectionViewSet(ModelViewSet):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    # Override built in delete method
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
+    # Override built in destroy method
+    def destroy(self, request, *args, **kwargs):
+        collection = self.get_object()
         if collection.products.count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # Override built in delete method
+    # def delete(self, request, pk):
+    #     collection = get_object_or_404(Collection, pk=pk)
+    #     if collection.products.count() > 0:
+    #         return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Collection List with generic class based view
 # class CollectionList(ListCreateAPIView):
