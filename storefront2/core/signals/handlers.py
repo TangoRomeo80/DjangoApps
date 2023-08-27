d2ef9f775bc6393525b5d6ba473526d5b241155e
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-08-27 12:57:52 +0600

    completed storefront 2

--- storefront2/core/signals/handlers.py
+++ storefront2/core/signals/handlers.py
@@ -1,6 +1,7 @@
 from store.signals import order_created
 from django.dispatch import receiver
 
+# used to decouple the code and make it more reusable
 @receiver(order_created)
 def on_order_created(sender, **kwargs):
     print(kwargs['order'])
