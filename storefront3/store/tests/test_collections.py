01e8e5b5287d718129c31565b090b6abb7fb6776
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2023-08-30 15:31:20 +0600

    finished automated testing section

--- storefront3/store/tests/test_collections.py
+++ storefront3/store/tests/test_collections.py
@@ -1,17 +1,79 @@
 # Tests for collections
 from rest_framework import status
-from rest_framework.test import APIClient
+# from rest_framework.test import APIClient
 import pytest
+from store.models import Collection
+from model_bakery import baker
+
+
+@pytest.fixture
+def crate_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
+
 
 @pytest.mark.django_db
 class TestCreateCollection:
     # @pytest.mark.skip(reason="Not implemented") # Skip test
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, crate_collection):
         # AAA - Arrange, Act, Assert
         # Arrange
-        
+        # client = APIClient()
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        # response = api_client.post('/store/collections/', {'title': 'a'})
+        response = crate_collection({'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, crate_collection):
+        # AAA - Arrange, Act, Assert
+        # Arrange
+        # client = APIClient()
+        authenticate(is_staff=False)
+        # Act
+        # esponse = api_client.post('/store/collections/', {'title': 'a'})
+        response = crate_collection({'title': 'a'})
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, crate_collection):
+        # AAA - Arrange, Act, Assert
+        # Arrange
+        # client = APIClient()
+        authenticate(is_staff=True)
+        # Act
+        # response = api_client.post('/store/collections/', {'title': ''})
+        response = crate_collection({'title': ''})
+        # Assert
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self, authenticate, crate_collection):
+        # AAA - Arrange, Act, Assert
+        # Arrange
+        # client = APIClient()
+        authenticate(is_staff=True)
+        # Act
+        # response = api_client.post('/store/collections/', {'title': 'a'})
+        response = crate_collection({'title': 'a'})
+        # Assert
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collections_exists_return_200(self, api_client):
+        # AAA - Arrange, Act, Assert
+        # Arrange
+        collection = baker.make(Collection)
+        # Act
+        response = api_client.get(f'/store/collections/{collection.id}/')
+        # Assert
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        } 
