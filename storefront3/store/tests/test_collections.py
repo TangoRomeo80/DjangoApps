c4a2487c2fa136c68399feba0a8197b0d1fbaff6
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2023-08-30 14:44:32 +0600

    completed upto running continuous tests

--- storefront3/store/tests/test_collections.py
+++ storefront3/store/tests/test_collections.py
@@ -1,8 +1,11 @@
 # Tests for collections
-from rest_framework.test import status
+from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
+@pytest.mark.django_db
 class TestCreateCollection:
+    # @pytest.mark.skip(reason="Not implemented") # Skip test
     def test_if_user_is_anonymous_returns_401(self):
         # AAA - Arrange, Act, Assert
         # Arrange
@@ -10,6 +13,5 @@ class TestCreateCollection:
         # Act
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
-
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
